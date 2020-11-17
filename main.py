269e57a998359928d0b4a31803463e61c3ea61ab
mtirado1 <mtirado1999@hotmail.com>
2020-11-16 19:55:47 -0800

    Add GUI

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import audio
 import signals
 import time
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
-audio.start()
-f = 500
-while True:
-    last_t = audio.t
-    audio.audioFunction = lambda t: 0.5 * signals.sinewave(f, t) * signals.decay(2, t-last_t)
-    f = int(input('Enter frequency: '))
+oscillators = {'Sine Wave': signals.sinewave}
 
-audio.stop()
-audio.terminate()
+class Main(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self, parent = None)
+        uic.loadUi('main.ui', self)
+        self.setWindowTitle('Digital Synthetizer')
+
+        self.osc1Type.addItems(list(oscillators.keys()))
+        self.osc2Type.addItems(list(oscillators.keys()))
+
+        self.osc1Gain.valueChanged.connect(self.updateValues)
+        self.osc1Freq.valueChanged.connect(self.updateValues)
+
+    def updateValues(self):
+        self.osc1GainLabel.setText('Gain: ' + str(self.osc1Gain.value()/100*0.5))
+        self.osc1FreqLabel.setText('Frequency: ' + str(self.osc1Freq.value()/200*4000) + ' Hz')
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = Main()
+    window.show()
+    audio.start()
+    audio.audioFunction = lambda t: window.osc1Gain.value()/100 * 0.5 * signals.sinewave(window.osc1Freq.value()/200 * 4000, t) * signals.sinewave(2, t)
+    sys.exit(app.exec_())
+    audio.stop()
+    audio.terminate()
